8463ec23be937f955ab2f60aba3d2825e7a306b7
kevinmorenom <is714714@iteso.mx>
2021-02-26 17:10:12 -0600

    Added cart folder

---
+++ lib/cart/item_cart.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ItemCart extends StatefulWidget {
+  ItemCart({Key key}) : super(key: key);
+
+  @override
+  _ItemCartState createState() => _ItemCartState();
+}
+
+class _ItemCartState extends State<ItemCart> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(),
+    );
+  }
+}
